a2439ed888d985ec9c858b71c33b0fa7e0f77722
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-09-03 14:19:58 +0200

    style login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Sign in</h1>
-<a href="/auth/github">Sign in with GitHub</a>
+
+<main>
+	<section class="content-tile">
+		<h1>Sign in</h1>
+		<div class="sign-ins">
+			<a role="button" href="/auth/github">
+				<i class="ri-github-fill"></i>
+				<span>Sign in with GitHub</span>
+			</a>
+		</div>
+	</section>
+</main>
+
+<style lang="postcss">
+	main {
+		@apply flex flex-col items-center justify-center h-screen;
+	}
+
+	h1 {
+		@apply text-3xl font-bold mb-7;
+	}
+
+	i {
+		@apply mr-3 text-2xl;
+	}
+
+	div.sign-ins {
+		@apply flex flex-col items-center;
+	}
+	a {
+		@apply flex items-center justify-between w-full;
+		@apply px-4 py-2 bg-gray-800 text-white rounded-md;
+	}
+	a:hover {
+		@apply bg-gray-900;
+	}
+</style>

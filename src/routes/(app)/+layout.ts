c0ccc70345d5d163aadb87d90503a26c8899ceab
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-09-09 15:48:36 +0200

    check for authentication in app layout

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -1,11 +1,13 @@
 import type { APIUserData } from '$domain/account/exports';
+import { isAuthenticated } from '$lib/helpers/api';
 import type { LayoutLoad } from './$types';
 
 export const ssr = true;
 export const prerender = false;
 
-export const load: LayoutLoad = async ({ fetch }) => {
+export const load: LayoutLoad = async ({ fetch, url }) => {
 	const userRes = await fetch('/account/user');
+	isAuthenticated(userRes, url);
 	const user = (await userRes.json()) as APIUserData;
 
 	return {

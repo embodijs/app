a88eb7a099487160a4c5422cab6837c21cabb494
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-22 16:53:58 +0100

    export for compoents and add section
    sections to give parts a frame

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './organisms/Header.svelte';
+export { default as OAuthButton } from './organisms/OAuthButton.svelte';
+export { default as Section } from './atoms/Section.svelte';
+export { default as Toaster } from './organisms/Toaster.svelte';

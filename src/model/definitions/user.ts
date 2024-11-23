136acc515e0d5c95110675c1861f3684e53af86d
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-23 17:04:38 +0100

    remove null from user type

--- src/model/definitions/user.ts
+++ src/model/definitions/user.ts
@@ -3,7 +3,7 @@ import type { TYPEID, TypeId } from '$lib/typeid';
 import type { InferSelectModel } from 'drizzle-orm';
 import type { SessionValidationResult } from '$infra/auth/lucia.server';
 
-type User = SessionValidationResult['user'];
+type User = NonNullable<SessionValidationResult['user']>;
 
 export type UserId = TypeId<TYPEID.USER>;
 export type UserDatabase = InferSelectModel<typeof user>;

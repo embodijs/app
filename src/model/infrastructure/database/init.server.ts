57f267ee2a66303878ecf12b782f15ad93677ea1
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-24 18:36:21 +0100

    load database url from .env

--- src/model/infrastructure/database/init.server.ts
+++ src/model/infrastructure/database/init.server.ts
@@ -1,6 +1,11 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-const client = createClient({ url: 'file:embodi-local.db' });
+import { env } from '$env/dynamic/private';
+
+if (!env.DATABASE_URL) {
+	throw new Error('DATABASE_URL is not set');
+}
+const client = createClient({ url: env.DATABASE_URL });
 export const db = drizzle(client);
 
 export default db;
